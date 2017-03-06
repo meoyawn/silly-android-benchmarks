a8bc98a477d9d8f64c6d9589da1d1645235e4931
adelnizamutdinov <stiggpwnz@gmail.com>
2017-03-06 22:23:21 +0300

    initial

---
+++ README.md
@@ -0,0 +1,10 @@
+json-benchmarks
+===============
+
+time:
+
+![](https://i.imgur.com/u0JFmNa.png)
+
+gc count:
+
+![](https://i.imgur.com/fcoFpQK.png)
---
+++ app/src/androidTest/kotlin/adeln/json/types.kt
@@ -0,0 +1,58 @@
+package adeln.json
+
+data class LfmResults<out T>(
+    val results: T
+)
+
+data class AlbumSearch(
+    val albummatches: AlbumMatches
+)
+
+data class AlbumMatches(
+    val album: List<LfmAlbum>
+)
+
+data class LfmAlbum(
+    val mbid: String,
+    val artist: String,
+    val name: String,
+    val image: List<LfmImage>
+)
+
+data class LfmImage(
+    val size: String
+)
+
+data class LfmTag(
+    val name: String,
+    val url: String
+)
+
+data class Tags(
+    val tag: List<LfmTag>
+)
+
+data class Tracks(
+    val track: List<LfmTrack>
+)
+
+data class LfmArtist(
+    val mbid: String,
+    val name: String,
+    val url: String
+)
+
+data class LfmTrack(
+    val artist: LfmArtist,
+    val duration: String,
+    val name: String,
+    val url: String
+)
+
+data class LfmAlbumInfo(
+    val artist: String,
+    val image: List<LfmImage>,
+    val tags: Tags,
+    val tracks: Tracks,
+    val url: String
+)
